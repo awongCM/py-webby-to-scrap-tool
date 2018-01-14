8129c0858818bbc4c5a0502713f363705fa0f8ac
awongCM <awong.cm@gmail.com>
2018-01-14 16:05:57 +1100

    imported csv and html scrapying libraries; implemented search semantic tag elements on the page for links, images, paragraph and titles

--- py-webby-to-scrap-tool.py
+++ py-webby-to-scrap-tool.py
@@ -1,47 +1,94 @@
-# library to query website using standard HTTP protocol
+# import libraries I need
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import webbrowser
+
+from datetime import datetime
+import csv
+
 '''
 TODOS
 A tool to takes a url content and scrap 3 data infomration on the page.
 h1 - heading
 url - url link
 p - paragraph
+img - images
 
 '''
-from urllib.request import urlopen
 
-from bs4 import BeautifulSoup
+base_url = "https://www.sbs.com.au/popasia/blog/2017/08/23/youtube-helping-push-k-pop-mainstream"
 
-wiki = "https://www.billboard.com/articles/columns/k-town/7873405/mrshll-gay-artist-korea-kpop"
-
-#query website and return html to the variable
-page = urlopen(wiki)
+#query website and return html to the page
+page = urlopen(base_url)
 
 soup = BeautifulSoup(page)
 
-print(soup.prettify())
+# what you see the actual html content
+html_str = soup.prettify()
+print(html_str)
 
+# then write the content to a local html file
+html_file = open('base_url.html', 'w')
+html_file.write(html_str)
+html_file.close()
+
+# Write to new file
+try:
+  file_name = "file:///Users/andywongcm/Documents/Github/py-webby-to-scrap-tool/base_url.html"
+  webbrowser.open_new_tab(file_name)
+except:
+  print("Cannot open local file: {0}".format(file_name))
+
+
+def decide_output_type(output_str, csv_file_writer=None):
+  if csv_file_writer is not None:
+    csv_file_writer.writerow([output_str])
+  else:
+    print(output_str)
+
+def search_and_output_tag(of_tag_type, title, attr_type, csv_file_writer=None):
+  main_title = "=== {0} ===".format(title)
+  decide_output_type(main_title, csv_file_writer)
+  all_tags = soup.find_all(of_tag_type)
+  for tag in all_tags:
+    tag_output_str = determine_type(tag, attr_type)
+    decide_output_type(tag_output_str, csv_file_writer)
+  total_tag_count_str = "Total {0} items: {1}".format(of_tag_type.upper(), len(all_tags))
+  decide_output_type(total_tag_count_str, csv_file_writer)
+
+def determine_type(tag_obj, tag_type):
+  output_str = None
+  if tag_type == "text":
+    output_str = "Content: {0}".format(tag_obj.text)
+  elif tag_type == "href":
+    output_str = "Link: {0} ".format(tag_obj.get("href"))
+  elif tag_type == "image":
+    output_str = "Image: {0} , Alt: {1}".format(tag_obj.attrs['src'] , tag_obj.get('alt',''))
+  return output_str
+
+# Prints output to console screen
 print("==== Title ====")
-print(soup.title)
-print("=== h1 Titles ===")
-for h1_title in soup.find_all("h1"):
-  print("H1: %s" % h1_title.text)
+print(soup.title.text)
+search_and_output_tag("h1", "H1 Titles", "text")
+search_and_output_tag("h2", "H2 Titles", "text")
+search_and_output_tag("p", "paragraphs content", "text")
+search_and_output_tag("a", "a links", "href")
+search_and_output_tag("img", "images", "image")
 
-print("==== h2 Titles ====")
-for h2_title in soup.find_all("h2"):
-  print("H2: %s" % h2_title.text)
+# Writes to CSV report file
+with open('base_url_semantics.csv', 'a') as csv_file:
+  # Empty file contents first
+  csv_file.seek(0)
+  csv_file.truncate()
 
-print("==== paragraphs  content ====")
-all_paragraphs = soup.find_all("p")
-for paragraph in all_paragraphs:
-  print("Paragraph: %s " % paragraph.text)
-
-print("==== a links ====")
-all_links = soup.find_all("a")
-for link in all_links:
-  print("Links: %s " % link.get("href"))
-
-print("==== images ====")
-all_images = soup.find_all("img")
-for image in all_images:
-  print("Image: {0} , Alt: {1}".format(image.attrs['src'] , image.get('alt','')))
+  writer = csv.writer(csv_file)
+  
+  writer.writerow(["==== Title ===="])
+  writer.writerow([soup.title.text])
+  
+  search_and_output_tag("h1", "H1 Titles", "text", writer)
+  search_and_output_tag("h2", "H2 Titles", "text", writer)
+  search_and_output_tag("p", "paragraphs content", "text", writer)
+  search_and_output_tag("a", "a links", "href", writer)
+  search_and_output_tag("img", "images", "image", writer)
 
