a3d4857b574bc7b542c9f69ef5a81a318cdad57c
awongCM <awong.cm@gmail.com>
2018-06-04 23:30:57 +1000

    refactored the code for sample code

--- py-webby-to-scrap-tool.py
+++ py-webby-to-scrap-tool.py
@@ -7,6 +7,8 @@ import webbrowser
 from datetime import datetime
 import csv
 
+# Beautiful Soup Use Case Example
+
 base_url = "https://www.seek.com.au/software-developer-jobs/in-All-Sydney-NSW"
 
 #query website and return html to the page
@@ -14,75 +16,75 @@ page = urlopen(base_url)
 
 soup = BeautifulSoup(page)
 
-# what you see the actual html content
+# prettify the html string content 
 html_str = soup.prettify()
-print(html_str)
 
 # then write the content to a local html file
 html_file = open('base_url.html', 'w')
 html_file.write(html_str)
 html_file.close()
 
-# Write to new file
-try:
-  file_name = "file:///Users/andywongcm/Documents/Github/py-webby-to-scrap-tool/base_url.html"
-  webbrowser.open_new_tab(file_name)
-except:
-  print("Cannot open local file: {0}".format(file_name))
-
-
-def decide_output_type(output_str, csv_file_writer=None):
-  if csv_file_writer is not None:
-    csv_file_writer.writerow([output_str])
-  else:
-    print(output_str)
-
-def search_and_output_tag(of_tag_type, title, attr_type, csv_file_writer=None):
-  main_title = "=== {0} ===".format(title)
-  decide_output_type(main_title, csv_file_writer)
-  all_tags = soup.find_all(of_tag_type)
-  for tag in all_tags:
-    tag_output_str = determine_type(tag, attr_type)
-    decide_output_type(tag_output_str, csv_file_writer)
-  total_tag_count_str = "Total {0} items: {1}".format(of_tag_type.upper(), len(all_tags))
-  decide_output_type(total_tag_count_str, csv_file_writer)
-
-def determine_type(tag_obj, tag_type):
-  output_str = None
-  if tag_type == "text":
-    output_str = "Content: {0}".format(tag_obj.text)
-  elif tag_type == "href":
-    output_str = "Link: {0} ".format(tag_obj.get("href"))
-  elif tag_type == "image":
-    output_str = "Image: {0} , Alt: {1}".format(tag_obj.attrs['src'] , tag_obj.get('alt',''))
-  return output_str
+# Open up the newly written html file
+# try:
+#   file_name = "file:///Users/andywongcm/Documents/Github/py-webby-to-scrap-tool/base_url.html"
+#   webbrowser.open_new_tab(file_name)
+# except:
+#   print("Cannot open local file: {0}".format(file_name))
 
 def fetch_articles():
   return soup.find_all("article")
 
-def extract_details(a_job_article):
+def extract_details(a_job_article, csv_writer=None):
+  job_article_dict = {}
+
   # Job title
   print(a_job_article["aria-label"])
+  job_article_dict["job_title"] = a_job_article["aria-label"]
 
   # Company
   company = a_job_article.find(attrs={"data-automation":"jobCompany"}) 
   if company !=  None:
     print(company.text)
+    job_article_dict["company"] = company.text
 
   # Location
-  company = a_job_article.find(attrs={"data-automation":"jobCompany"}) 
-  if company !=  None:
-    print(company.text)
+  location = a_job_article.find(attrs={"data-automation":"jobLocation"}) 
+  if location !=  None:
+    print(location.text)
+    job_article_dict["location"] = location.text
 
-  # Job Type
-  company = a_job_article.find(attrs={"data-automation":"jobCompany"}) 
-  if company !=  None:
-    print(company.text)
+  # Area
+  area = a_job_article.find(attrs={"data-automation":"jobArea"}) 
+  if area !=  None:
+    print(area.text)
+    job_article_dict["area"] = area.text
   
+  # Salary range
+  salary = a_job_article.find(attrs={"data-automation":"jobSalary"}) 
+  if salary !=  None:
+    print(salary.text)
+    job_article_dict["salary_range"] = salary.text
+  
+  # Duties and tasks(optional)
+  if a_job_article.find('ul') != None:
+    dutiestasks_list = a_job_article.find('ul').find_all('li')
+    list_of_dutiestasks = []
+    if dutiestasks_list != None:  
+      for dutiestasks_item in dutiestasks_list:
+        if dutiestasks_item !=  None:
+          print(dutiestasks_item.text)
+          list_of_dutiestasks.append(dutiestasks_item.text)
+      job_article_dict["role_specification"] = ';'.join(list_of_dutiestasks)
+  else:
+    job_article_dict["role_specification"] = ""
+      
   # Job Description
-  company = a_job_article.find(attrs={"data-automation":"jobCompany"}) 
-  if company !=  None:
-    print(company.text)
+  job_description = a_job_article.find(attrs={"data-automation":"jobShortDescription"}) 
+  if job_description !=  None:
+    print(job_description.text)
+    job_article_dict["job_description"] = job_description.text
+
+  csv_writer.writerow(job_article_dict)
 
 # Writes to CSV report file
 with open('base_url_semantics.csv', 'a') as csv_file:
@@ -90,13 +92,12 @@ with open('base_url_semantics.csv', 'a') as csv_file:
   csv_file.seek(0)
   csv_file.truncate()
 
-  csv_writer = csv.writer(csv_file)
-  
-  csv_writer.writerow(["==== Title ===="])
-  csv_writer.writerow([soup.title.text])
+  fieldnames = ['job_title', 'company', 'location', 'area', 'salary_range', 'role_specification', 'job_description']
+  csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+  csv_writer.writeheader()
   
   job_articles = fetch_articles()
 
   for a_job_article in job_articles:
-    extract_details(a_job_article)
+    extract_details(a_job_article, csv_writer)
   
