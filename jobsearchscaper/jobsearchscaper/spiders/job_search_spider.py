db3e1cb8b2242f7ef61220b52814ac49f4be1a0e
awongCM <awong.cm@gmail.com>
2019-05-20 16:18:33 +1000

    added my todos in cod

--- jobsearchscaper/jobsearchscaper/spiders/job_search_spider.py
+++ jobsearchscaper/jobsearchscaper/spiders/job_search_spider.py
@@ -1,10 +1,13 @@
 import scrapy
 
 # Scrapy Use Case Example
+
+
 class JobSearchSpider(scrapy.Spider):
-    name="job_searches"
+    name = "job_searches"
     allowed_domains = ["seek.com.au"]
-    start_urls = ["https://www.seek.com.au/software-developer-jobs/in-All-Sydney-NSW"]
+    start_urls = [
+        "https://www.seek.com.au/software-developer-jobs/in-All-Sydney-NSW"]
 
     def parse(self, response):
         self.log('Browsing ' + response.url)
@@ -13,24 +16,38 @@ class JobSearchSpider(scrapy.Spider):
         for job in job_articles:
             list_of_dutiestasks = []
             duties_list = job.css('ul li')
+            # print('duties_list', duties_list)
             for each_duty in duties_list:
-                list_of_dutiestasks.append(each_duty.css('span[data-automation="false"]::text').extract_first())
-            
+                list_of_dutiestasks.append(each_duty.css(
+                    'span[data-automation="false"]::text').extract_first())  # TODO - time to revisit this xpath: -_-
+
             item = {}
-            item['job_title'] = job.css('article::attr(aria-label)').extract_first()
-            item['company'] = job.css('a[data-automation="jobCompany"]::text').extract_first()
-            item['location'] = job.css('a[data-automation="jobLocation"]::text').extract_first()
-            item['area'] = job.css('a[data-automation="jobArea"]::text').extract_first()
-            item['salary_range'] = job.css('span[data-automation="jobSalary"] span::text').extract_first()
-            item['role_specification'] = ';'.join(list_of_dutiestasks)
-            item['job_description'] = job.css('span[data-automation="jobShortDescription"] span[data-automation="false"]::text').extract_first()
-            # Grab the actual job post link 
-            item['job_link'] = response.urljoin(job.css('a[data-automation="jobTitle"]::attr(href)').extract_first())
+            item['job_title'] = job.css(
+                'article::attr(aria-label)').extract_first()
+            item['company'] = job.css(
+                'a[data-automation="jobCompany"]::text').extract_first()
+            item['location'] = job.css(
+                'a[data-automation="jobLocation"]::text').extract_first()
+            item['area'] = job.css(
+                'a[data-automation="jobArea"]::text').extract_first()
+            item['salary_range'] = job.css(
+                'span[data-automation="jobSalary"] span::text').extract_first()
+            # TODO - fix up error handling when list of duties and tasks can't be found
+            if len(list_of_dutiestasks) != 0:
+                item['role_specification'] = ';'.join(list_of_dutiestasks)
+            else:
+                item['role_specification'] = ''
+            item['job_description'] = job.css(
+                'span[data-automation="jobShortDescription"] span[data-automation="false"]::text').extract_first()
+            # Grab the actual job post link
+            item['job_link'] = response.urljoin(
+                job.css('a[data-automation="jobTitle"]::attr(href)').extract_first())
 
             yield item
 
         # following paginations
-        next_page_url = response.css('a[data-automation="page-next"]::attr(href)').extract_first()
+        next_page_url = response.css(
+            'a[data-automation="page-next"]::attr(href)').extract_first()
         self.log("Next page url to navigate: " + next_page_url)
         if next_page_url:
             next_page_url = response.urljoin(next_page_url)
