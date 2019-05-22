390b62d52cb4672571559571a6f3d441fe1193d2
awongCM <awong.cm@gmail.com>
2019-05-22 14:23:08 +1000

    added first unit test file

---
+++ jobsearchscaper/jobsearchscaper/__test__/__init__.py
@@ -0,0 +1,13 @@
+# TODOs
+import unittest
+
+
+class JobSearchSpiderTestCase(unittest.TestCase):
+    """docstring for JobSearchSpiderTestCase."""
+
+    def test_demo(self):
+        print('test demo')
+
+
+if __name__ == "__main__":
+    unittest.main()
