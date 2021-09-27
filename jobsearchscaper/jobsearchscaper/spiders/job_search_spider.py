7fc29b6c4d7b90987495ce9b4ca61bdb43e89a10
awongCM <awong.cm@gmail.com>
2021-09-27 10:45:12 +1000

    removed old code

--- jobsearchscaper/jobsearchscaper/spiders/job_search_spider.py
+++ jobsearchscaper/jobsearchscaper/spiders/job_search_spider.py
@@ -7,7 +7,6 @@ class JobSearchSpider(scrapy.Spider):
     name = "job_searches"
     allowed_domains = ["seek.com.au"]
     start_urls = [
-        # "https://www.seek.com.au/software-developer-jobs/in-All-Sydney-NSW?sortmode=ListedDate" -- old
         "https://www.seek.com.au/lead-developer-jobs/in-All-Sydney-NSW"
     ]
 
