f9e443846eaf74fb9f7fa3a701820ebb9bc37b05
awongCM <awong.cm@gmail.com>
2021-10-23 14:05:18 +1100

    implemented one of the oop classes

--- htmlsoup-scraper.py
+++ htmlsoup-scraper.py
@@ -1,22 +1,39 @@
-# TODO - OOD class to implement for BeautifulSoup
+# TODO - OOD class
+# 1. One for reading the locally written html
+# 2. One for parsing html content and writing its CSV content
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import webbrowser
 
-from datetime import datetime
 import csv
 
+base_url = "https://www.seek.com.au/lead-developer-jobs/in-All-Sydney-NSW"
+
+
 class HTMLSoupScraper:
-  
-  def __init__(self, baseurl):
-    raise NotImplementedError
-  
-  def open_url(self, parameter_list):
-    pass
+    html_output = "main_url.html"
 
-  def output_html_content(self, parameter_list):
-    pass
+    def __init__(self, baseurl):
+        self.baseurl = baseurl
+        self.page_content = ''
 
-  def write_content_to_file(parameter_list):
-    pass
+    def open_url(self):
+        return BeautifulSoup(urlopen(self.baseurl), features="lxml")
+
+    def output_html_content(self):
+        soup = self.open_url()
+        self.page_content = soup.prettify()
+
+    def write_content_to_file(self, option):
+        html_file = open(self.html_output, option)
+        html_file.write(self.page_content)
+        html_file.close()
+
+    def __str__(self):
+        return f'{self.html_output} will be printed'
+
+
+if __name__ == "__main__":
+    html_soup_scraper = HTMLSoupScraper(base_url)
+    html_soup_scraper.output_html_content()
+    html_soup_scraper.write_content_to_file('w')
